fbb1b5d0a60389c4fd5b95d7ddd0a0f39e058475
Felipe Beserra <49963588+Beserrovsky@users.noreply.github.com>
2021-03-16 12:41:17 -0300

    Chroma command added

--- src/services/message-responder.ts
+++ src/services/message-responder.ts
@@ -2,21 +2,29 @@ import {Message} from "discord.js";
 import {PingFinder} from "./ping-finder";
 import {inject, injectable} from "inversify";
 import {TYPES} from "../types";
+import { Chroma } from "./chroma";
 
 @injectable()
 export class MessageResponder {
   private pingFinder: PingFinder;
+  private chroma: Chroma;
 
   constructor(
-    @inject(TYPES.PingFinder) pingFinder: PingFinder
+    @inject(TYPES.PingFinder) pingFinder: PingFinder,
+    @inject(TYPES.Chroma) chroma: Chroma,
   ) {
     this.pingFinder = pingFinder;
+    this.chroma = chroma;
   }
 
   handle(message: Message): Promise<Message | Message[]> {
 
     if (this.pingFinder.isPing(message.content)) {
         return message.reply('pong!');
+    }
+
+    if (this.chroma.isChroma(message.content)){
+        return this.chroma.handle(message);
     }
 
     return Promise.reject();
